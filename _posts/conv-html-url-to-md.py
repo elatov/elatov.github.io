f9ef68b3652530ce4c072363808f97f701516276
Karim Elatov <elatov@github.com>
2014-05-23 10:16:36 -0600

    pics

--- _posts/conv-html-url-to-md.py
+++
@@ -1,6 +0,0 @@
-import fileinput
-import re
- 
-for line in fileinput.input(inplace=1):
-    line = re.sub(r'http\:\/\/virtuallyhyper.com\/wp-content\/uploads/(.*)', r'https://github.com/elatov/uploads/raw/master/\1', line.rstrip())
-    print(line)
